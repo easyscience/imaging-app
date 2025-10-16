b21cd988e37c06cffbf57659730bbca23933f1de
Christian Vedel <christian.vedel@ess.eu>
2025-10-16 16:11:17 +0200

    Add QtInstaller folders

---
+++ Installer/packages/core/meta/installscript.js
@@ -0,0 +1,127 @@
+/****************************************************************************
+**
+** Copyright (C) 2017 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the FOO module of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:GPL-EXCEPT$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+var Dir = new function () {
+    this.toNativeSparator = function (path) {
+        if (systemInfo.kernelType === "winnt")
+            return path.replace(/\//g, '\\');
+        return path;
+    }
+};
+
+function Component() {
+    // constructor
+    if (installer.isInstaller()) {
+        component.loaded.connect(this, Component.prototype.loaded);
+    }
+}
+
+Component.prototype.loaded = function () {
+    if (installer.addWizardPage(component, "CustomIntroductionPage", QInstaller.TargetDirectory)) {
+        var page = gui.pageByObjectName("DynamicCustomIntroductionPage");
+        if (page != null) {
+            page.entered.connect(Component.prototype.dynamicCustromIntroductionPageEntered);
+        }
+    }
+    console.log("component loaded");
+    if (systemInfo.kernelType === "winnt") {
+        console.log("System is windows")
+        if (installer.addWizardPage(component, "ShortcutWidget", QInstaller.StartMenuSelection)) {
+            var widget = gui.pageWidgetByObjectName("DynamicShortcutWidget");
+            if (widget != null) {
+                widget.createDesktopShortcut.checked = true;
+                widget.createStartMenuShortcut.checked = true;
+
+                widget.windowTitle = "Create shortcuts";
+            }
+        }
+    } else if (systemInfo.kernelType == "linux") {
+        console.log("System is linux");
+        if (installer.addWizardPage(component, "SymlinkWidget", QInstaller.StartMenuSelection)) {
+            var widget = gui.pageWidgetByObjectName("DynamicSymlinkWidget");
+            if (widget != null) {
+                widget.createDesktopShortcut.checked = true;
+                widget.createUserLink.checked = true;
+                widget.createSystemLink.checked = false;
+
+                widget.windowTitle = "Create shortcuts";
+            }
+        }
+    }
+}
+
+Component.prototype.dynamicCustromIntroductionPageEntered = function ()
+{
+    var pageWidget = gui.pageWidgetByObjectName("DynamicCustomIntroductionPage");
+    if (pageWidget != null) {
+        pageWidget.m_pageLabel.text = "Welcome to the EasyImaging setup.";
+        installer.setDefaultPageVisible(QInstaller.Introduction, false);
+    }
+}
+
+Component.prototype.createOperations = function()
+{
+    try {
+        // call the base create operations function
+        component.createOperations();
+    } catch (e) {
+        console.log(e);
+    }
+	
+    if (systemInfo.kernelType === "winnt") {
+        var shortcutpage = component.userInterface("ShortcutWidget");
+        if (shortcutpage && shortcutpage.createDesktopShortcut.checked) {
+            component.addElevatedOperation("CreateShortcut", "@TargetDir@/EasyImaging.exe", "@DesktopDir@/EasyImaging.lnk");
+        }
+        if (shortcutpage && shortcutpage.createStartMenuShortcut.checked) {
+            component.addElevatedOperation("CreateShortcut", "@TargetDir@/EasyImaging.exe", "@UserStartMenuProgramsPath@/@StartMenuDir@/EasyImaging.lnk",
+                "workingDirectory=@TargetDir@");
+        }   
+    } else if (systemInfo.kernelType === "linux") {
+        var shortcutpage = component.userInterface("SymlinkWidget");
+        if (shortcutpage && shortcutpage.createDesktopShortcut.checked) {
+            component.addOperation("CreateDesktopEntry", "@HomeDir@/.local/share/applications/EasyImaging.desktop", 
+                "Type=Application\n"+
+                 "Name=EasyImaging\n"+
+                 "Comment=Neutron imaging Bragg-edge analysis software\n"+
+                 "Exec=@TargetDir@/EasyImaging\n"+
+                 "Icon=@TargetDir@/_internal/Gui/Resources/Logos/App.svg\n"+
+                 "Categories=Science"
+                     );
+
+        }
+        if (shortcutpage && shortcutpage.createUserLink.checked) {
+            console.log("Creating Symlinks in @HomeDir@/.local/bin");
+            component.addOperation("CreateLink", "@HomeDir@/.local/bin/easyimaging", "@TargetDir@/EasyImaging");
+        }
+        if (shortcutpage && shortcutpage.createSystemLink.checked) {
+            console.log("Creating Symlinks in @RootDir@usr/local/bin");
+            component.addElevatedOperation("CreateLink", "@RootDir@usr/local/bin/easyimaging", "@TargetDir@/EasyImaging");
+        }
+    }
+}
